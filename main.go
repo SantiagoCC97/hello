1dd66d82455623169a58bbdc8975cfa3d880b239
SantiagoCC97 <scastro932@misena.edu.co>
2023-12-04 18:00:07 -0500

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/hello/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
+
 }
